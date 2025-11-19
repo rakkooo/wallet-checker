754d541dae70e2e1e0736cd9d3f268e41c89beb6
unknown <tsb16b@gmail.com>
2025-11-19 13:19:53 +0900

    fix postcss plugin shape

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,8 +3,8 @@ import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
 
 export default {
-  plugins: {
-    tailwindcss,
-    autoprefixer,
-  },
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
 }
